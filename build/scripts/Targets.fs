76bb6237958784f33d746d924c51c2f2ac9dcd1c
Steve Gordon <sgordon@hotmail.co.uk>
2020-11-16 16:45:45 +0000

    Update second run command

--- build/scripts/Targets.fs
+++ build/scripts/Targets.fs
@@ -80,7 +80,7 @@ let private generateReleaseNotes (arguments:ParseResults<Arguments>) =
 let private createReleaseOnGithub (arguments:ParseResults<Arguments>) =
     let project = Paths.RootRelative Paths.ToolProject.FullName
     let currentVersion = currentVersion.Value
-    let dotnetRun =[ "run"; "-c"; "Release"; "-f"; "netcoreapp3.1"; "-p"; project]
+    let dotnetRun =[ "run"; "-c"; "Release"; "-f"; "net5.0"; "-p"; project]
     let tokenArgs =
         match arguments.TryGetResult Token with
         | None -> []
